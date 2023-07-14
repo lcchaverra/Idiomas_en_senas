5872bd5e820d5ebdf8841c5aed5bbe0d5b9e74eb
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-07-14 11:23:26 -0500

    update de menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,28 +1,72 @@
-const menu = ({isMenuOpen}:any) => {
-
-    const scrollToSection = (event:any, sectionId:any) => {
-      event.preventDefault();
-      const section = document.getElementById(sectionId);
-      if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-      }
-    };
+const menu = ({ isMenuOpen }: any) => {
+  const scrollToSection = (event: any, sectionId: any) => {
+    event.preventDefault();
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <>
-    <nav className={`main-nav ${isMenuOpen ? 'show' : ''}`} id="main-nav">
+      <nav className={`main-nav ${isMenuOpen ? "show" : ""}`} id="main-nav">
         <ul className="main-menu">
-        {/* <li className="main-menu__item"><Link to="/" className="main-menu__link">Inicio</Link></li>
+          {/* <li className="main-menu__item"><Link to="/" className="main-menu__link">Inicio</Link></li>
             <li className="main-menu__item"><Link to="/contact" className="main-menu__link">Contacto</Link></li> */}
-          <li className="main-menu__item"><a className="main-menu__link" href="#ini" onClick={(event) => scrollToSection(event, 'ini')}> Inicio </a> </li>
-          <li className="main-menu__item"><a className="main-menu__link" href="#services" onClick={(event) => scrollToSection(event, 'services')}> Servicios </a> </li>
-          <li className="main-menu__item"><a className="main-menu__link" href="#activities" onClick={(event) => scrollToSection(event, 'activities')}> Actividades </a> </li>
-          <li className="main-menu__item"><a className="main-menu__link" href="#gallery" onClick={(event) => scrollToSection(event, 'gallery')}> Galeria </a> </li>
-          <li className="main-menu__item"><a className="main-menu__link" href="#contact" onClick={(event) => scrollToSection(event, 'contact')}> Contactanos </a> </li>
+          <li className="main-menu__item">
+            <a
+              className="main-menu__link"
+              href="#ini"
+              onClick={(event) => scrollToSection(event, "ini")}
+            >
+              {" "}
+              Inicio{" "}
+            </a>{" "}
+          </li>
+          <li className="main-menu__item">
+            <a
+              className="main-menu__link"
+              href="#services"
+              onClick={(event) => scrollToSection(event, "services")}
+            >
+              {" "}
+              Servicios{" "}
+            </a>{" "}
+          </li>
+          <li className="main-menu__item">
+            <a
+              className="main-menu__link"
+              href="#activities"
+              onClick={(event) => scrollToSection(event, "activities")}
+            >
+              {" "}
+              Actividades{" "}
+            </a>{" "}
+          </li>
+          <li className="main-menu__item">
+            <a
+              className="main-menu__link"
+              href="#gallery"
+              onClick={(event) => scrollToSection(event, "gallery")}
+            >
+              {" "}
+              Galeria{" "}
+            </a>{" "}
+          </li>
+          <li className="main-menu__item">
+            <a
+              className="main-menu__link"
+              href="#contact"
+              onClick={(event) => scrollToSection(event, "contact")}
+            >
+              {" "}
+              Contactanos{" "}
+            </a>{" "}
+          </li>
         </ul>
-    </nav>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default menu
+export default menu;
