848b8dcea7b935f267fcf7e88b97cc06ec9db199
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-08-15 23:44:58 -0500

    test logo 2

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import Menu from "./menu";
-// import Logo from "/src/assets/Logo.svg";
+import Logo2 from "/src/assets/Logo.svg";
 import Logo from "/src/assets/Logomenu2.svg";
 import { useState } from 'react';
 
@@ -16,7 +16,7 @@ const header = () => {
       <header className="main-header">
         <div className="main-logo">
           <a href="/">
-            <img src={Logo} alt="Logo de Idiomas en Señas" />
+            <img src={Logo2} alt="Logo de Idiomas en Señas" />
           </a>
         </div>
 
