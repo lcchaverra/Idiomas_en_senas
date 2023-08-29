0434a487cb2ad6aac2c150706d9a6fbd04df48e8
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-08-29 17:10:25 -0500

    update interest

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -4,29 +4,36 @@ import ArgentinaFlag from '../assets/icons/argentina.png';
 import FranceFlag from '../assets/icons/france.png';
 
 interface Option {
+  id: number;
   label: string;
   value: string;
   flag: string;
 }
 
 const options: Option[] = [
-  { label: "Lengua de Señas Francesas", value: "Lengua de Señas Francesas", flag: FranceFlag  },
-  { label: "Lengua de Señas Mexicana", value: "Lengua de Señas Mexicana", flag: MexicoFlag },
-  { label: "Lengua de Señas Argentina", value: "Lengua de Señas Argentina" , flag: ArgentinaFlag },
+  { id:1 ,label: "Lengua de Señas Francesas", value: "Lengua de Señas Francesas", flag: FranceFlag  },
+  { id:2 ,label: "Lengua de Señas Mexicana", value: "Lengua de Señas Mexicana", flag: MexicoFlag },
+  { id:3 ,label: "Lengua de Señas Argentina", value: "Lengua de Señas Argentina" , flag: ArgentinaFlag },
 ];
 
 const Interests: React.FC = () => {
+
+  const selectedOptionRef = useRef(null)
+
+  const voteHandle: any = () => {
+    console.log("su voto", selectedOptionRef.current)
+  }
 
   return (
     <>
       <div className="interest-container">
         <h3>¿Qué idiomas te interesa aprender en la lengua de señas?</h3>
         <div className="interest-grid-container">
-          <select className="interest-select">
+          <select className="interest-select" ref={selectedOptionRef}>
             {
               options.map(option => (
-                <option className="interest-item" value={option.value}>{option.label}
-                  <img src={option.flag} alt="Bandera de la opcion a escoger" />
+                <option key={option.id} className="interest-item" value={option.value}>{option.label}
+                  {/* <img src={option.flag} alt="Bandera de la opcion a escoger" /> */}
                 </option>
                 ))
             }
@@ -38,7 +45,7 @@ const Interests: React.FC = () => {
             </q>
         </div>
       </div>
-          <button className="btn">Votar</button>
+          <button className="btn" onClick={voteHandle}>Votar</button>
           <p>El objetivo es ampliar cupos en la lengua de señas en otros idiomas</p>
         </div>
     </>
