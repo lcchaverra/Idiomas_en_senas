e3a3a0cd21c54338740d48a2de12e6d8b414c040
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-10-16 22:22:49 -0500

    update contact form responsive mode

--- src/components/MultiuserPackages.tsx
+++ src/components/MultiuserPackages.tsx
@@ -1,5 +1,4 @@
 import Giftest from "../assets/gif/señas1.gif";
-import CloudImg from "../assets/icons/nube.png";
 
 const MultiuserPackages = () => {
   return (
@@ -22,7 +21,6 @@ const MultiuserPackages = () => {
                   <li>
                     Puedes acceder a los cursos 24/7 en cualquier hora y momento
                   </li>
-                  {/* <img src={CloudImg} alt="Nube de fondo para los beneficios" /> */}
                 </div>
                 <div className="cloud-item">
                   <li>
@@ -39,41 +37,6 @@ const MultiuserPackages = () => {
               </ul>
             </div>
           </div>
-
-          {/* <div className="packages-users">
-            <h3> Cursos para Programas </h3>
-
-            <div className="packages-gif-container">
-              <img
-                src={Giftest}
-                alt="Gif indicativo para los paquetes multiusuarios"
-              />
-            </div>
-            <div className="packages-user-content">
-              <h4> Beneficio </h4>
-
-              <ul>
-                <div className="cloud-item">
-                  <li>
-                    Puedes acceder a los cursos 24/7 en cualquier hora y momento
-                  </li>
-                </div>
-                <div className="cloud-item">
-                  <li>
-                    Puedes practicar y jugar con contenido interactivo de
-                    aceurdo a tu curiosidad
-                  </li>
-                </div>
-                <div className="cloud-item">
-                  <li>
-                    Puedes interactuar con personas subscritas en cursos para
-                    practicar lo aprendido desde cualquier parte del mundo
-                  </li>
-                </div>
-              </ul>
-            </div>
-          </div> */}
-          
         </div>
       </div>
     </>
